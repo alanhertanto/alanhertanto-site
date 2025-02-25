d57306af91967f06df37c1ee6e9412a4b47d0905
alan-nusameta <alan.hertanto@nusameta.io>
2025-02-25 15:56:26 +0700

    Add wallpaper

--- app/components/PhoneFrame.js
+++ app/components/PhoneFrame.js
@@ -3,7 +3,7 @@ import HomeButton from "./HomeButton";
 export default function PhoneFrame({ children }) {
     return (
         <div className="flex justify-center items-center min-h-screen bg-gray-900">
-            <div className="relative w-[375px] h-[812px] bg-black rounded-[40px] shadow-lg overflow-hidden">
+            <div className="relative w-[375px] h-[812px] bg-black rounded-[25px] shadow-lg overflow-hidden border-4 border-gray-600">
                 {children}
                 <HomeButton /> 
             </div>

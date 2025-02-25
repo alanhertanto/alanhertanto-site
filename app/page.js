34e24641a9759ec8b7d4799ede3c8ed14d815986
alan-nusameta <alan.hertanto@nusameta.io>
2025-02-25 15:56:26 +0700

    feat(ui): add wallpaper background

--- app/page.js
+++ app/page.js
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <PhoneFrame>
       <Navbar />
-      <div className="h-full flex flex-col justify-center items-center text-white text-xl font-semibold">
-        <h1>Welcome to My Portfolio</h1>
+      <div className="h-full flex flex-col justify-center items-center text-white text-xl font-semibold ">
+        <h1>Hello! I am Alan Hertanto Guspermana. <br></br>A Fullstack Developer & Game Developer</h1>
         <div className="mt-4 space-x-4">
           <Link href="/portfolio" className="px-4 py-2 bg-blue-500 rounded-lg">
             Portfolio

e891b4fd3878d0199cdea1f1db45c2c4e4ca31f7
alan-nusameta <alan.hertanto@nusameta.io>
2025-02-28 23:10:53 +0700

    feat(widgets): add weather, calendar, and two apps

--- app/page.js
+++ app/page.js
@@ -1,14 +1,18 @@
+"use client";
 import PhoneFrame from "./components/PhoneFrame";
 import Navbar from "./components/Navbar";
 import BezelBottom from "./components/BezelBottom";
 import AppsBottom from "./components/AppsBottom";
+import CalendarWeather from "./components/widget/calendar and weather/page";
+import AppsCollection from "./components/AppsCollection";
 
 export default function Home() {
   return (
     <PhoneFrame>
       <Navbar />
       <div className="h-full flex flex-col mt-20 mb-20 items-start justify-start p-4 text-white text-xl font-semibold">
-
+        <CalendarWeather />
+        <AppsCollection />
       </div>
       <AppsBottom />
       <BezelBottom />

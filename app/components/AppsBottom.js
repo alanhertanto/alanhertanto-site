85921b48955ec9c20e8e4eafa84cc29e6ad86369
alan-nusameta <alan.hertanto@nusameta.io>
2025-05-19 20:23:43 +0700

    feat(guestbook): add guest book feature

--- app/components/AppsBottom.js
+++ app/components/AppsBottom.js
@@ -2,6 +2,11 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { PhoneIcon, EnvelopeIcon, UserIcon, PhotoIcon } from "@heroicons/react/24/solid";
+import Head from 'next/head';
+
+<Head>
+    <script src="https://unpkg.com/mailgo@0.12.1/dist/mailgo.min.js" defer></script>
+</Head>
 
 export default function AppsBottom() {
     const router = useRouter();
@@ -37,21 +42,33 @@ export default function AppsBottom() {
         });
 
         setLaunching(true);
-        setTimeout(() => window.location.href=path, 700); // Navigate after animation
+        setTimeout(() => window.location.href = path, 700); // Navigate after animation
     };
+
+    const handleMailClick = () => {
+        const email = "alanhertantog@gmail.com";
+        const cc = "alanhertantog@gmail.com";
+        const bcc = "alanhertantog@gmail.com";
+        const subject = encodeURIComponent("A strange email");
+        const body = encodeURIComponent("This email is for me with me also in cc and in bcc");
+
+        window.location.href = `mailto:${email}?cc=${cc}&bcc=${bcc}&subject=${subject}&body=${body}`;
+    };
+
 
     return (
         <>
             {/* Bottom Navigation */}
             <div className="grid grid-cols-4 gap-4 w-full bottom-0 left-0 absolute mb-20 ml-1">
                 <button className="relative w-16 h-16 flex items-center justify-center rounded-2xl bg-gradient-to-b from-yellow-400 to-yellow-600 shadow-lg"
-                    onClick={(e) => handleClickExternalLink(e,'https://wa.me/6289676277362') }>
+                    onClick={(e) => { handleClickExternalLink(e, 'https://wa.me/6289676277362') }}>
                     <PhoneIcon className="w-8 h-8 text-white drop-shadow-lg" />
                 </button>
                 <button className="relative w-16 h-16 flex items-center justify-center rounded-2xl bg-gradient-to-b from-yellow-400 to-yellow-600 shadow-lg"
-                    onClick={(e) => handleClick(e, "/message")}>
+                    onClick={(e) => handleClick(e, "/guest-book")}>
                     <EnvelopeIcon className="w-8 h-8 text-white drop-shadow-lg" />
                 </button>
+
                 <button className="relative w-16 h-16 flex items-center justify-center rounded-2xl bg-gradient-to-b from-yellow-400 to-yellow-600 shadow-lg"
                     onClick={(e) => handleClick(e, "/about")}>
                     <UserIcon className="w-8 h-8 text-white drop-shadow-lg" />

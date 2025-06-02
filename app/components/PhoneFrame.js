011e08746830c2e5e8feb1331ad82a196a38a192
alan-nusameta <alan.hertanto@nusameta.io>
2025-06-02 13:22:11 +0700

    aa

--- app/components/PhoneFrame.js
+++ app/components/PhoneFrame.js
@@ -5,8 +5,8 @@ export default function PhoneFrame({ children }) {
 
     return (
         <>
-            <ParticlesBackground />
             <div className="flex justify-center items-center min-h-screen bg-gray-900">
+            <ParticlesBackground />
                 <div className={`flex w-[380px] h-[820px] rounded-[55px] overflow-hidden border-4 border-gray-600`}>
                     <div className={`relative w-[375px] h-[812px] wallpaper1 rounded-[50px] shadow-lg overflow-hidden border-8 border-gray-800`}>
                         {children}

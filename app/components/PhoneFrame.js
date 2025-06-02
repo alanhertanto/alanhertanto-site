1fa08a2a85279f6763264e86f51135c3751189dd
alan-nusameta <alan.hertanto@nusameta.io>
2025-06-02 13:37:31 +0700

    style: remove background

--- app/components/PhoneFrame.js
+++ app/components/PhoneFrame.js
@@ -5,7 +5,7 @@ export default function PhoneFrame({ children }) {
 
     return (
         <>
-            <div className="flex justify-center items-center min-h-screen bg-gray-900">
+            <div className="flex justify-center items-center min-h-screen">
             <ParticlesBackground />
                 <div className={`flex w-[380px] h-[820px] rounded-[55px] overflow-hidden border-4 border-gray-600`}>
                     <div className={`relative w-[375px] h-[812px] wallpaper1 rounded-[50px] shadow-lg overflow-hidden border-8 border-gray-800`}>

e2ea2e6e79516b519e15adf2fe35dfdee0108135
alan-nusameta <alan.hertanto@nusameta.io>
2025-06-04 11:10:00 +0700

    adding left and right

--- app/components/PhoneFrame.js
+++ app/components/PhoneFrame.js
@@ -19,8 +19,7 @@ export default function PhoneFrame({ children }) {
             {/* Left Handwritten SVG */}
             {!isAndroid && (
                <div className="absolute left-0 text-white max-w-[250px] px-4">
-                    <h1 className="text-2xl font-bold mb-2">Alan Hertanto Guspermana</h1>
-                    <h2 className="text-lg font-medium text-gray-300">Versatile developer specializing in Unity, WebGL, VR, AR, and multiplayer networking, with strong skills in backend development, AI integration, and asset optimization. Dedicated to crafting polished, engaging interactive experiences across platforms.</h2>
+                    <h2 className="text-2xl font-bold mb-2">Alan Hertanto Guspermana</h2>
                 </div>
             )}
 
@@ -35,12 +34,7 @@ export default function PhoneFrame({ children }) {
             {/* Right Handwritten SVG */}
             {!isAndroid && (
                 <div className="absolute right-0 text-white max-w-[250px] px-4">
-                    <svg viewBox="0 0 600 300" className="w-full h-auto">
-                        <path
-                            d="M10,30 C40,0,80,60,120,30" // simplify or break into multiple paths
-                            className="handwriting-path delay-[1s]"
-                        />
-                    </svg>
+                    <h2 className="text-lg font-medium text-gray-300">Versatile developer specializing in Unity, WebGL, VR, AR, and multiplayer networking, with strong skills in backend development, AI integration, and asset optimization. Dedicated to crafting polished, engaging interactive experiences across platforms.</h2>
                 </div>
             )}
 
